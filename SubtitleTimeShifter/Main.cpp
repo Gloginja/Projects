d8e8493f176a0af87a81e5170e55b5585e8fee2d
Gloginja <amarko00@hotmail.com>
2021-11-29 23:11:06 +0100

    Fixed

--- SubtitleTimeShifter/Main.cpp
+++ SubtitleTimeShifter/Main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 int main() {
 
 	//List<int>Lista();
-	SRT* test = new SRT("primer.srt",-1);
+	SRT* test = new SRT("primer.srt",1);
 	test->loadFile();
 	test->shift();
 	test->saveFile();
 	
+
 	return 0;
 }
